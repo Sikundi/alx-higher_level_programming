af15816551f8f1e98ba88f74272d97fd1282929a
Sikundi <sikundi499@gmail.com>
2023-07-20 10:39:11 +0300

    Script that creates MYSQLserver user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
--- Script to run msql commands
+-- script to run msql commands
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON *.* TO uaer_0d_1@localhost
+GRANT ALL ON *.* TO user_0d_1@localhost
